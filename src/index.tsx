1d352e7803949c421678aff62096aaae25543398
Wincentaina <103582986+Wincentaina@users.noreply.github.com>
2024-05-17 21:22:37 +0700

    feat: донастроенны стили

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,10 @@
-import LanguageProvider from 'providers/LanguageProvider';
-import ThemeProvider from 'providers/ThemeProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import './fonts/YS/stylesheet.css';
-import { App } from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </ThemeProvider>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
