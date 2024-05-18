eac5caa7d47f69959fae4cff0da7de174733f16d
Wincentaina <103582986+Wincentaina@users.noreply.github.com>
2024-05-18 12:38:26 +0700

    fix: удален из index.tsx fonts

--- src/components/HeaderComponent/NavLinkComponent/NavLinkComponent.tsx
+++ src/components/HeaderComponent/NavLinkComponent/NavLinkComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {INavLinkComponentProps} from "../../../types/HeaderComponentProps/NavLinkComponentProps/INavLinkComponentProps";
-import s from "NavLinkComponent.module.scss";
+import s from "./NavLinkComponent.module.scss";
 import cn from "classnames";
 
 export const NavLinkComponent: React.FC<INavLinkComponentProps> = ({icon, text, isActive}) => {
